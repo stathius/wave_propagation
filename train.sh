e65f0e3adc5c38a5300c7f06a4ddd98a581a00d6
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-05-27 20:06:55 +0100

    add scripts for server

---
+++ train.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --partition=MSC
+#SBATCH --gres=gpu:2
+#SBATCH --mem=12000  # memory in Mb
+#SBATCH --time=0-10:00:00
+
+export CUDA_HOME=/opt/cuda-9.0.176.1/
+
+export CUDNN_HOME=/opt/cuDNN-7.0/
+
+export STUDENT_ID=$(whoami)
+
+export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+mkdir -p /disk/scratch/${STUDENT_ID}
+
+export TMPDIR=/disk/scratch/${STUDENT_ID}/
+export TMP=/disk/scratch/${STUDENT_ID}/
+
+mkdir -p ${TMP}/datasets/
+export DATASET_DIR=${TMP}/datasets/
+
+rsync -ua --progress /home/${STUDENT_ID}/wave_propagation/Video_Data/ /disk/scratch/${STUDENT_ID}/Video_Data
+
+source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
+python main.py

682d76df58b9de995b1fe99249959c7adbc19153
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-06-04 22:58:45 +0100

    rename file

--- test_network.py
+++ test_network.py
@@ -1,41 +1,48 @@
+import torch
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from utils.io import figure_save
-from utils.Scorekeeper import Scorekeeper
+import random
+import math
+import numpy as np
+from PIL import Image
+import copy
+import logging
+from utils.io import imshow
 
-def test(test_data_loader, score_keeper, plot=True, channels=3):
+def test(model, test_dataloader, num_input_frames, num_output_frames, channels, device, score_keeper, results_dir, plot=True):
     """
     Testing of network
-    :param test_data_loader: Data to test
-    :param plot: If to plot predictions
+    :param test_dataloader: Data to test
+    :param plot: If to plot predictionss
     :return:
     """
     def initial_input(No_more_Target):
-        output = model(ImageSeries.to(device))
+        output = model(images_series.to(device))
         try:
-            target = OriginalSeries[:, (t0 + cnt + input_frames) * channels:(t0 + cnt + input_frames + 1) * channels, :, :].to(device)
+            target = batch[:, (t0 + cnt + num_input_frames) * channels:(t0 + cnt + num_input_frames + 1) * channels, :, :].to(device)
         except:
             No_more_Target = True
             target = None
         return output, target, No_more_Target
 
-    def new_input(output, target, No_more_Target):
-        output = torch.cat((output, model(ImageSeries, mode="new_input")), dim=1)
+    def reinsert(output, target, No_more_Target):
+        output = torch.cat((output, model(images_series, mode="reinsert")), dim=1)
         try:
             target = torch.cat(
-                (target, OriginalSeries[:, (t0 + cnt + input_frames) * channels:(t0 + cnt + input_frames + 1) * channels, :, :].to(device)
+                (target, batch[:, (t0 + cnt + num_input_frames) * channels:(t0 + cnt + num_input_frames + 1) * channels, :, :].to(device)
                  ), dim=1)
         except:
             No_more_Target = True
         return output, target, No_more_Target
 
-    def consequent_propagation(output, target, No_more_Target):
-        if n < (output_frames - refeed_offset):
-            output = torch.cat((output, model(torch.Tensor([0]), mode="internal")), dim=1)
+    def propagate(output, target, No_more_Target):
+        if n < (num_output_frames - refeed_offset):
+            output = torch.cat((output, model(torch.Tensor([0]), mode="propagate")), dim=1)
             try:
                 target = torch.cat(
-                    (target, OriginalSeries[:, (t0 + cnt + input_frames) * channels:(t0 + cnt + input_frames + 1) * channels, :, :].to(device)
+                    (target, batch[:, (t0 + cnt + num_input_frames) * channels:(t0 + cnt + num_input_frames + 1) * channels, :, :].to(device)
                 ), dim=1)
             except:
                 No_more_Target = True
@@ -43,13 +50,13 @@ def test(test_data_loader, score_keeper, plot=True, channels=3):
 
     def plot_predictions():
         if (total == 0) & (n == 0) & (run == 0):
-            for imag in range(int(ImageSeries.shape[1] / channels)):
+            for imag in range(int(images_series.shape[1] / channels)):
                 fig = plt.figure().add_axes()
                 sns.set(style="white")  # darkgrid, whitegrid, dark, white, and ticks
                 sns.set_context("talk")
-                imshow(ImageSeries[selected_batch, imag * channels:(imag + 1) * channels, :, :], title="Input %01d" % imag, obj=fig)
-                figure_save(maindir1 + "Input %02d" % imag)
-        if (total == 0) & (n < (output_frames - refeed_offset)):
+                imshow(images_series[selected_batch, imag * channels:(imag + 1) * channels, :, :], title="Input %01d" % imag, obj=fig)
+                figure_save(results_dir + "Input %02d" % imag)
+        if (total == 0) & (n < (num_output_frames - refeed_offset)):
             predicted = output[selected_batch, -channels:, :, :].cpu()
             des_target = target[selected_batch, -channels:, :, :].cpu()
             fig = plt.figure()
@@ -59,7 +66,7 @@ def test(test_data_loader, score_keeper, plot=True, channels=3):
             imshow(predicted, title="Predicted %02d" % cnt, smoothen=True, obj=pred)
             tar = fig.add_subplot(1, 2, 2)
             imshow(des_target, title="Target %02d" % target_cnt, obj=tar)
-            figure_save(maindir1 + "Prediction %02d" % cnt)
+            figure_save(results_dir + "Prediction %02d" % cnt)
             plt.show() if plot else plt.close()
 
     def plot_cutthrough(frequently_plot=5, direction="Horizontal", location=None):
@@ -67,24 +74,18 @@ def test(test_data_loader, score_keeper, plot=True, channels=3):
             intensity = []
             location = []
             hue = []
-            if "orizontal" in direction:
-                for i in range(np.shape(img1)[1]):
-                    intensity.append(img1[stdmax[0], i, 0])
-                    location.append(i)
-                    hue.append(hue1)
-                for i in range(np.shape(img2)[1]):
-                    intensity.append(img2[stdmax[0], i, 0])
-                    location.append(i)
-                    hue.append(hue2)
-            elif "ertical" in direction:
-                for i in range(np.shape(img1)[0]):
-                    intensity.append(img1[i, stdmax[0], 0])
-                    location.append(i)
-                    hue.append(hue1)
-                for i in range(np.shape(img2)[0]):
-                    intensity.append(img2[i, stdmax[0], 0])
-                    location.append(i)
-                    hue.append(hue2)
+            if "Horizontal" in direction:
+                intensity = np.append(img1[stdmax[0],:], img2[stdmax[0],:])
+                length1 = img1.shape[1]
+                length2 = img2.shape[1]
+                location = list(range(length1)) + list(range(length2))
+                hue = [hue1] * length1 + [hue2] * length2
+            elif "Vertical" in direction:
+                intensity = np.append(img1[:,stdmax[0]], img2[:,stdmax[0]])
+                width1 = img1.shape[0]
+                width2 = img2.shape[0]
+                location = list(range(width1)) + list(range(width2))
+                hue = [hue1] * width1 + [hue2] * width2
 
             data_dict = {"Intensity": intensity, "Pixel Location": location, "Image": hue}
             #g = sns.FacetGrid(pd.DataFrame.from_dict(data_dict), col="Image")
@@ -108,30 +109,34 @@ def test(test_data_loader, score_keeper, plot=True, channels=3):
                 predicted = imshow(predicted, title="Predicted %02d" % cnt, return_np=True, obj=pre)
                 des_target = imshow(des_target, title="Target %02d" % target_cnt, return_np=True, obj=tar)
                 if not location:
-                    if "orizontal" in direction:
+                    if "Horizontal" in direction:
                         std = np.std(des_target, axis=1)
-                    elif "ertical" in direction:
+                    elif "Vertical" in direction:
                         std = np.std(des_target, axis=0)
-                    stdmax, _ = np.where(std.max() == std)
+                    stdmax = np.where(std.max() == std)
                 else:
                     stdmax = location
 
-                if "orizontal" in direction:
+                if "Horizontal" in direction:
                     pre.plot([0, np.shape(std)[0]], [stdmax[0], stdmax[0]], color="yellow")
                     tar.plot([0, np.shape(std)[0]], [stdmax[0], stdmax[0]], color="yellow")
-                elif "ertical" in direction:
+                elif "Vertical" in direction:
                     pre.plot([stdmax[0], stdmax[0]], [0, np.shape(std)[0]], color="yellow")
                     tar.plot([stdmax[0], stdmax[0]], [0, np.shape(std)[0]], color="yellow")
 
                 cutthrough(predicted, des_target, "Predicted", "Target")
-                figure_save(maindir1 + "Cut-through %02d" % cnt, obj=fig)
+                figure_save(results_dir + "Cut-through %02d" % cnt, obj=fig)
                 plt.show() if plot else plt.close()
 
-    def add_score():
-        if (not No_more_Target) & (n < (output_frames - refeed_offset)):
+
+
+
+    def add_score(score_keeper, output, target, num_output_frames, channels, cnt, No_more_Target):
+        if (not No_more_Target) & (n < (num_output_frames - refeed_offset)):
             for ba in range(output.size()[0]):
-                score_keeper.add(output[ba, -channels:, :, :].cpu(), target[ba, -channels:, :, :].cpu(), cnt,
-                                 "pHash", "SSIM", "Own", "RMSE")
+                score_keeper.add(output[ba, -channels:, :, :].cpu(), 
+                                 target[ba, -channels:, :, :].cpu(), 
+                                 cnt,"pHash", "pHash2", "SSIM", "Own", "RMSE")
 
     def introduce(prev_data):
         """
@@ -150,7 +155,7 @@ def test(test_data_loader, score_keeper, plot=True, channels=3):
             return matrix.squeeze_(0)
 
         prev_data = prev_data.cpu()
-        data = My_Test[0]["image"][t0 * channels: (t0 + input_frames) * channels, :, :]
+        data = My_Test[0]["image"][t0 * channels: (t0 + num_input_frames) * channels, :, :]
         for i in range(int(data.size()[0] / channels)):
             means = find_mean(data[i * channels:(i + 1) * channels, :, :])
             prev_data[selected_batch, i * channels:(i + 1) * channels, :, :] += data[i * channels:(i + 1) * channels, :, :] - means
@@ -162,33 +167,32 @@ def test(test_data_loader, score_keeper, plot=True, channels=3):
     t0 = 15 # Can be 0
     refeed_offset = 0
     selected_batch = random.randint(0, 15)
-    if (output_frames - refeed_offset) < input_frames:
-        refeed_offset = output_frames - input_frames
-    for batch_num, batch in enumerate(test_data_loader):
-        OriginalSeries = batch["image"]
-        ImageSeries = OriginalSeries[:, t0 * channels:(t0 + input_frames) * channels, :, :]
-        #ImageSeries = introduce(ImageSeries)
-        model.reset_hidden(ImageSeries.size()[0])
+    if (num_output_frames - refeed_offset) < num_input_frames:
+        refeed_offset = num_output_frames - num_input_frames
+    for batch_num, batch in enumerate(test_dataloader):
+        batch = batch["image"]
+        images_series = batch[:, t0 * channels:(t0 + num_input_frames) * channels, :, :]
+        model.reset_hidden(images_series.size()[0])
         No_more_Target = False
         cnt = target_cnt = 0
-        for run in range(int(m.ceil((100 - (t0 + input_frames + 1)) / (output_frames - refeed_offset)))):
+        for run in range(int(math.ceil((100 - (t0 + num_input_frames + 1)) / (num_output_frames - refeed_offset)))):
             if run != 0:
-                if (refeed_offset == 0) or ((output_frames - refeed_offset) <= input_frames):
-                    ImageSeries = output[:, -input_frames * channels:, :, :]
+                if (refeed_offset == 0) or ((num_output_frames - refeed_offset) <= num_input_frames):
+                    images_series = output[:, -num_input_frames * channels:, :, :]
                 else:
-                    ImageSeries = output[:, -(input_frames + refeed_offset) * channels:-refeed_offset * channels, :, :]
+                    images_series = output[:, -(num_input_frames + refeed_offset) * channels:-refeed_offset * channels, :, :]
                 cnt -= refeed_offset
-            for n in range(output_frames):
+            for n in range(num_output_frames):
                 if n == 0:
                     if run == 0:
                         output, target, No_more_Target = initial_input(No_more_Target)
                     else:
-                        output, target, No_more_Target = new_input(output, target, No_more_Target)
+                        output, target, No_more_Target = reinsert(output, target, No_more_Target)
                 else:
-                    output, target, No_more_Target = consequent_propagation(output, target, No_more_Target)
-                    # output & target size is [batches, 3 * (n + 1), 128, 128]
+                    output, target, No_more_Target = propagate(output, target, No_more_Target)
+                    # output & target size is [batches, channels * (n + 1), 128, 128]
 
-                add_score()
+                add_score(score_keeper, output, target, num_output_frames, channels, cnt, No_more_Target)
                 plot_predictions()
                 plot_cutthrough()
                 cnt += 1
@@ -196,9 +200,7 @@ def test(test_data_loader, score_keeper, plot=True, channels=3):
                     target_cnt = copy.copy(cnt)
 
         total += target.size()[0]
-        logging.info(batch_num + 1, "out of", len(test_data_loader))
+        logging.info("{:d} out of {:d}".format(batch_num + 1, len(test_dataloader)))
+        break
     logging.info("Correct: {}\tPercentile: {:.0f}%".format(correct, 100 * correct / total))
-    score_keeper.plot()
-
-score_keeper = Scorekeeper()
-test(Test_Data, score_keeper, plot=True, channels=channels)
+    score_keeper.plot(results_dir)

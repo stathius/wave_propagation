08332640cc5c64b73536cbfca2d8f5bd7d43b36f
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-06-11 21:39:06 +0100

    seed everything

---
+++ utils/various.py
@@ -0,0 +1,13 @@
+import torch
+import random
+import numpy as np
+import os
+
+
+def seed_everything(seed):
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.backends.cudnn.deterministic = True

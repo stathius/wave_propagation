d002e1d78edd8c409dda3ded216811b9fdd40a34
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-06-22 21:54:34 +0100

    seeding everything

--- utils/arg_extract.py
+++ utils/arg_extract.py
@@ -15,10 +15,10 @@ def str2bool(v):
 
 def seed_everything(seed):
     random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 

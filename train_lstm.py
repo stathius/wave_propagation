52093ebd5873df3776aad5a30c8bbdd36ff788f2
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-06-22 18:34:27 +0100

    remove load files

--- train_lstm.py
+++ train_lstm.py
@@ -10,7 +10,7 @@ import platform
 import time
 from utils.Network import Network
 from utils.Analyser import Analyser
-from utils.io import save_network, load_network, save, load, make_folder_results
+from utils.io import save_network, load_network, save, load, create_results_folder
 from utils.WaveDataset import create_datasets, transformVar, normalize
 from utils.training import train_epoch, validate, test
 from utils.arg_extract import get_args
@@ -25,76 +25,44 @@ logging.basicConfig(format='%(message)s',level=logging.INFO)
 
 args, device = get_args()
 
-if 'Darwin' in platform.system():
-    data_dir = './'
-else:
-    data_dir = '/disk/scratch/s1680171/wave_propagation/'
-
-if not os.path.isdir("./Results"):
-    os.mkdir("./Results")
-results_dir = "./Results/" + args.experiment_name 
-
-if not os.path.isdir(results_dir):
-    make_folder_results(results_dir)
-
 # Data
-filename_data = os.path.join(results_dir,"all_data.pickle")
-if os.path.isfile(filename_data):
-    logging.info('Loading datasets')
-    all_data = load(filename_data)
-    train_dataset = all_data["Training data"]
-    val_dataset = all_data["Validation data"]
-    test_dataset = all_data["Testing data"]
+if 'Darwin' in platform.system():
+    base_folder = '/Users/stathis/Code/thesis/wave_propagation'
 else:
-    logging.info('Creating new datasets')
-    test_dataset, val_dataset, train_dataset = create_datasets(
-         data_dir+"Video_Data/", transformVar, test_fraction=0.15, validation_fraction=0.15, check_bad_data=False, num_channels=args.num_channels)
-    all_data = {"Training data": train_dataset, "Validation data": val_dataset, "Testing data": test_dataset}
-    save(all_data, filename_data)
+    base_folder = '/disk/scratch/s1680171/wave_propagation/'
+
+results_dir = create_results_folder(base_folder=base_folder, experiment_name=args.experiment_name)
+
+logging.info('Creating new datasets')
+test_dataset, val_dataset, train_dataset = create_datasets(os.path.join(base_folder,"Video_Data/"), transformVar, 
+                                                            test_fraction=0.15, validation_fraction=0.15, check_bad_data=False, 
+                                                            num_channels=args.num_channels)
 train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 
+filename_data = os.path.join(results_dir,"all_data.pickle")
+all_data = {"Training data": train_dataset, "Validation data": val_dataset, "Testing data": test_dataset}
+save(all_data, filename_data)
+
 
 # analyser
 filename_analyser = os.path.join(results_dir,"analyser.pickle")
-if os.path.isfile(filename_analyser):
-    logging.info('Loading analyser')
-    analyser = load(filename_analyser)
-else:
-    logging.info('Creating analyser')
-    analyser = Analyser(results_dir)
+logging.info('Creating analyser')
+analyser = Analyser(results_dir)
 
 # Model
 filename_model = os.path.join(results_dir,"model.pt")
-if os.path.isfile(filename_model):
-    model = Network(args.num_channels)
-    model = load_network(model, filename_model)
-else:
-    model = Network(args.num_channels)
-
+model = Network(args.num_channels)
 model.to(device)
 
 optimizer_algorithm = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4)
-# Add learning rate schedulers
-# Decay LR by a factor of gamma every step_size epochs
-scheduler_type = 'plateau'
-if scheduler_type == 'step':
-    gamma = 0.5
-    step_size = 40
-    lr_scheduler = optim.lr_scheduler.StepLR(optimizer_algorithm, step_size=step_size, gamma=gamma)
-elif scheduler_type == 'plateau':
-    # Reduce learning rate when a metric has stopped improving
-    lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer_algorithm, mode='min', factor=0.1, patience=7)
+lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer_algorithm, mode='min', factor=0.1, patience=7)
 
 # Save metadata
 filename_metadata = os.path.join(results_dir,"metadata.pickle" )
-meta_data_dict = {  "args": args,
-                    "optimizer": optimizer_algorithm.state_dict(),
-                    "scheduler_type": scheduler_type, 
-                    "scheduler": lr_scheduler.state_dict()}
+meta_data_dict = {  "args": args, "optimizer": optimizer_algorithm.state_dict(), "scheduler": lr_scheduler.state_dict()}
 save(meta_data_dict, filename_metadata)
-
 
 if __name__ == "__main__":
     logging.info('Experiment %s' % args.experiment_name)
@@ -115,17 +83,9 @@ if __name__ == "__main__":
         validation_loss = validate(model, val_dataloader, args.num_input_frames, args.num_output_frames, args.reinsert_frequency, 
                                     args.num_channels, device, show_plots=args.show_plots, debug=args.debug)
         analyser.save_validation_loss(validation_loss, epoch)
-        """
-        Here we can access analyser.validation_loss to make decisions
-        """
-        # Learning rate scheduler
-        # perform scheduler step if independent from validation loss
-        if scheduler_type == 'step':
-            lr_scheduler.step()
-        # perform scheduler step if dependent on validation loss
-        if scheduler_type == 'plateau':
-            validation_loss = analyser.validation_loss[-1]
-            lr_scheduler.step(validation_loss)
+        # perform pateau scheduler step dependent on validation loss
+        validation_loss = analyser.validation_loss[-1]
+        lr_scheduler.step(validation_loss)
         save_network(model, filename_model)
         save(analyser, filename_analyser)
 

f6db616f2856a4af84fd4e2fa258726b967544bd
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-06-26 23:34:48 +0100

    Get the transforms from a function. 3 differente normalizers in total

--- utils/WaveDataset.py
+++ utils/WaveDataset.py
@@ -7,22 +7,27 @@ from PIL import Image
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
-normalize = {'mean': 0.5047, 'std': 0.1176}
-# normalize = {'mean':0, 'std':1}
 
-transformVar = {"Test": transforms.Compose([
-    transforms.Resize(128),  # Already 184 x 184
-    transforms.CenterCrop(128),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[normalize['mean']], std=[normalize['std']])
-]), "Train": transforms.Compose([
-    transforms.Resize(128),  # Already 184 x 184
-    transforms.CenterCrop(128),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomVerticalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[normalize['mean']], std=[normalize['std']])])
-}
+normalizers = {'none': {'mean': 0.0, 'std': 1.0},  # leave as is
+               'normal': {'mean': 0.5047, 'std': 0.1176},  # mean 0 std 1
+               'm1to1': {'mean': 0.5, 'std': 0.5}  # makes it -1, 1
+               }
+
+
+def get_transform(normalize):
+    trans = {"Test": transforms.Compose([
+        transforms.Resize(128),  # Already 184 x 184
+        transforms.CenterCrop(128),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[normalize['mean']], std=[normalize['std']])
+    ]), "Train": transforms.Compose([
+        transforms.Resize(128),  # Already 184 x 184
+        transforms.CenterCrop(128),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[normalize['mean']], std=[normalize['std']])])}
+    return trans
 
 
 def open_image(filename, grayscale=False):

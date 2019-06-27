fc3cdaa0552555e04aec92cf9112387838c7a4ba
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-06-27 02:03:56 +0100

    Train convlstm works with experiment_setup

--- train_convlstm.py
+++ train_convlstm.py
@@ -1,36 +1,20 @@
 import torch.optim as optim
-from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
-import os
-import platform
 import logging
 from collections import OrderedDict
 from models.ConvLSTM import EncoderForecaster, Encoder, Forecaster, ConvLSTMCell
-from utils.io import save_network, save, create_results_folder, save_datasets_to_file
 from utils.arg_extract import get_args
-from utils.ExperimentBuilder import ExperimentBuilder
-from utils.WaveDataset import create_datasets, transformVar, normalize, create_dataloaders
+from utils.experiment_runner import ExperimentRunner
+from utils.experiment_setup import ExperimentSetup
+
 plt.ioff()
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 
-args, device = get_args()  # get arguments from command line
-
-# Data
-if 'Darwin' in platform.system():
-    base_folder = '/Users/stathis/Code/thesis/wave_propagation/'
-    data_dir = base_folder
-else:
-    base_folder = '/home/s1680171/wave_propagation/'
-    data_dir = '/disk/scratch/s1680171/wave_propagation/'
-
-dirs = create_results_folder(base_folder=base_folder, experiment_name=args.experiment_name)
-
-logging.info('Creating datasets')
-train_dataset, val_dataset, test_dataset = create_datasets(os.path.join(data_dir, "Video_Data/"), transformVar, test_fraction=0.15, validation_fraction=0.15)
-filename_data = os.path.join(dirs['pickles'], "all_data.pickle")
-save_datasets_to_file(train_dataset, val_dataset, test_dataset, filename_data)
-
-train_dataloader, val_dataloader, test_dataloader = create_dataloaders(train_dataset, val_dataset, test_dataset, args.batch_size, args.num_workers)
+args = get_args()
+setup = ExperimentSetup(args)
+dirs = setup.get_dirs()
+data_loaders, normalizer = setup.get_dataloaders()
+device = setup.get_device()
 
 
 # Define encoder #
@@ -75,24 +59,18 @@ encoder = Encoder(encoder_architecture[0], encoder_architecture[1]).to(device)
 forecaster = Forecaster(forecaster_architecture[0], forecaster_architecture[1], args.num_output_frames).to(device)
 model = EncoderForecaster(encoder, forecaster)
 
-optimizer = optim.Adam(model.parameters(), amsgrad=False, lr=args.learning_rate, weight_decay=args.weight_decay_coefficient)
+optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args.learning_rate)
 lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=7)
 
-# Save metadata
-filename_metadata = os.path.join(dirs['pickles'], "metadata.pickle")
-meta_data_dict = {"args": args, "optimizer": optimizer.state_dict(), "scheduler": lr_scheduler.state_dict(), "model": "%s" % model}
-save(meta_data_dict, filename_metadata)
-logging.info(meta_data_dict)
-
-experiment = ExperimentBuilder(model=model, lr_scheduler=lr_scheduler,
-                               experiment_name=args.experiment_name,
-                               num_epochs=args.num_epochs,
-                               samples_per_sequence=args.samples_per_sequence,
-                               device=device,
-                               train_data=train_dataloader,
-                               val_data=val_dataloader,
-                               test_data=test_dataloader,
-                               dirs=dirs,
-                               continue_from_epoch=-1,
-                               debug=args.debug)
+experiment = ExperimentRunner(model=model, lr_scheduler=lr_scheduler,
+                             experiment_name=args.experiment_name,
+                             num_epochs=args.num_epochs,
+                             samples_per_sequence=args.samples_per_sequence,
+                             device=device,
+                             train_data=data_loaders['train'],
+                             val_data=data_loaders['val'],
+                             test_data=data_loaders['test'],
+                             dirs=dirs,
+                             continue_from_epoch=-1,
+                             debug=args.debug)
 experiment.run_experiment()

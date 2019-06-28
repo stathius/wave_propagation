a5559cd59dd0388728cfcb4d4185bc5a2a52224c
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-06-28 23:52:58 +0100

    bug fixes and changes to ar test

--- test_ar_lstm.py
+++ test_ar_lstm.py
@@ -1,7 +1,7 @@
 import logging
 import torch
 import matplotlib.pyplot as plt
-from models.AR_LSTM import AR_LSTM, test
+from models.AR_LSTM import AR_LSTM, test_ar_lstm
 from utils.arg_extract import get_args_test
 from utils.Scorekeeper import Scorekeeper
 from utils.experiment_setup import ExperimentSetup, get_device, load_metadata, load_datasets, load_network, create_dataloaders, get_normalizer
@@ -15,9 +15,9 @@ setup = ExperimentSetup(args.experiment_name)
 metadata = load_metadata(setup.files['metadata'])
 print(metadata)
 # if metadata['args'].normalizer
-normalizer = get_normalizer(args.normalizer)
+normalizer = get_normalizer(args.normalizer_type)
 datasets = load_datasets(setup.files['datasets'])
-datasets['Testing data'].root_dir = setup.dirs['video_data']
+datasets['Testing data'].root_dir = setup.dirs['data']
 data_loaders = create_dataloaders(datasets, args.batch_size, args.num_workers)
 device = get_device()
 
@@ -28,5 +28,5 @@ model.to(device)
 score_keeper = Scorekeeper(setup.dirs['charts'], normalizer)
 
 with torch.no_grad():
-    test(model, data_loaders['test'], args.test_starting_point, args.num_input_frames, args.reinsert_frequency, device, score_keeper, setup.dirs['predictions'], show_plots=args.show_plots, debug=args.debug, normalize=normalizer)
+    test_ar_lstm(model, data_loaders['test'], args.test_starting_point, args.num_input_frames, args.reinsert_frequency, device, score_keeper, setup.dirs['predictions'], show_plots=args.show_plots, debug=args.debug, normalize=normalizer)
 score_keeper.plot(show_plots=args.show_plots)

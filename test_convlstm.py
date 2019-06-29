03be30ae03e8172ad7f4ceba725f52e6c21cbe86
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-06-29 16:15:13 +0100

    correct predictions folder

--- test_convlstm.py
+++ test_convlstm.py
@@ -29,10 +29,9 @@ model = get_convlstm_model(metadata['args'].num_input_frames, metadata['args'].n
 model = load_network(model, setup.files['model'])
 model.to(device)
 
-logging.info("Start testing")
 score_keeper = Scorekeeper(setup.dirs['charts'], normalizer)
-figures_dir = os.path.join(setup.dirs['results'], 'test_charts')
 
+logging.info("Start testing")
 with torch.no_grad():
-    test_convlstm(model, data_loaders['test'], args.test_starting_point, device, score_keeper, figures_dir, args.show_plots, debug=args.debug, normalize=normalizer)
+    test_convlstm(model, data_loaders['test'], args.test_starting_point, device, score_keeper, setup.dirs['predictions'], args.show_plots, debug=args.debug, normalize=normalizer)
 score_keeper.plot(args.show_plots)

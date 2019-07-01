4e10efd73b011a2315aab154959e2eb1c6f1250e
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-07-02 00:44:28 +0100

    remove print

--- utils/experiment.py
+++ utils/experiment.py
@@ -210,7 +210,7 @@ class Experiment():
         else:
             self.dirs['base'] = '/home/s1680171/wave_propagation/'
             self.dirs['data'] = '/disk/scratch/s1680171/wave_propagation/Video_Data/'
-        print(self.dirs)
+
         self.dirs['exp_folder'] = os.path.join(self.dirs['base'], "experiments_results/")
         self.dirs['results'] = os.path.join(self.dirs['exp_folder'], self.args.experiment_name)
         for d in self.sub_folders:

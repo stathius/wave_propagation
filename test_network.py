b5076b3d8184c1d1feeb877cc7fb5ad7c8aa32d6
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-07-02 22:35:04 +0100

    adding test network

--- test_network.py
+++ test_network.py
@@ -1,7 +1,7 @@
-import matplotlib.pyplot as plt
 import logging
+import matplotlib.pyplot as plt
 from utils.arg_extract import get_args
-from utils.experiment_evaluator import Evaluator, get_sample_predictions
+from utils.experiment_evaluator import Evaluator, test_future_frames
 from utils.experiment import Experiment
 
 plt.ioff()
@@ -11,8 +11,9 @@ args = get_args()
 experiment = Experiment(args)
 experiment.load_from_disk()
 
-evaluator = Evaluator(args.test_starting_point, args. num_total_output_frames, experiment.normalizer)
+evaluator = Evaluator(experiment.normalizer)
 
-evaluator.get_experiment_metrics(experiment)
-evaluator.save_plots(experiment.dirs['charts'])
-evaluator.save(experiment.files['evaluator'])
+logging.info("Start testing")
+evaluator.compute_experiment_metrics(experiment, args.test_starting_point, args.num_total_output_frames, debug=args.debug)
+# evaluator.save_plots(experiment.dirs['charts'])
+# evaluator.save(experiment.files['evaluator'])

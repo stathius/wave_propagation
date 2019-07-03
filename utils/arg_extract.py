ee30c6b023d66a8393a6a561b702a54012eda2ff
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-07-03 20:56:01 +0100

    rearranging test arguments

--- utils/arg_extract.py
+++ utils/arg_extract.py
@@ -37,14 +37,11 @@ def get_args():
     parser.add_argument('--num_output_frames', type=int, default=20, help='LSTM. How many framres to predict in the future"')
     parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--samples_per_sequence', type=int, default=10, help='how may training points to generate from a video sequence')
-    parser.add_argument('--test_starting_point', type=int, default=15, help='which frame to start the test')
-    parser.add_argument('--num_total_output_frames', type=int, default=80, help='how many frames to predict to the future')
     parser.add_argument('--reinsert_frequency', type=int, default=10, help='AR LSTM: how often to use the reinsert mechanism')
-    parser.add_argument('--num_output_keep_frames', type=int, default=20, help='ConvLSTM: How many frames to keep from one pass to continue autoregression for longer outputs')
     parser.add_argument('--experiment_name', type=str, default="dummy",
                         help='Experiment name - to be used for building the experiment folder')
     parser.add_argument('--normalizer_type', type=str, default='normal', help='how to normalize the images [normal, m1to1, none]')
-    parser.add_argument('--num_workers', type=int, default=12, help='how many workers for the dataloader')
+    parser.add_argument('--num_workers', type=int, default=8, help='how many workers for the dataloader')
     parser.add_argument('--seed', type=int, default=12345, help='Seed to use for random number generator for experiment')
     parser.add_argument('--seed_everything', type=str2bool, default=True)
     parser.add_argument('--debug', type=str2bool, default=False)
@@ -53,6 +50,10 @@ def get_args():
     parser.add_argument('--scheduler_patience', type=int, default=7, help='Epoch patience before reducing learning_rate')
     parser.add_argument('--scheduler_factor', type=float, default=0.1, help='Factor to reduce learning_rate')
     parser.add_argument('--continue_experiment', type=str2bool, default=False, help='Whether the experiment should continue from the last epoch')
+    # TESTING
+    parser.add_argument('--test_starting_point', type=int, default=15, help='which frame to start the test')
+    parser.add_argument('--num_total_output_frames', type=int, default=80, help='how many frames to predict to the future')
+    parser.add_argument('--num_output_keep_frames', type=int, default=20, help='ConvLSTM: How many frames to keep from one pass to continue autoregression for longer outputs')
 
     args = parser.parse_args()
 

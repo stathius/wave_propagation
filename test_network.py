e80728a5a3d9c871b04343950037fb252c73c270
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-07-03 01:08:15 +0100

    using evaluator to test network

--- test_network.py
+++ test_network.py
@@ -1,7 +1,7 @@
 import logging
 import matplotlib.pyplot as plt
 from utils.arg_extract import get_args
-from utils.experiment_evaluator import Evaluator, test_future_frames
+from utils.experiment_evaluator import Evaluator, get_sample_predictions
 from utils.experiment import Experiment
 
 plt.ioff()
@@ -11,9 +11,11 @@ args = get_args()
 experiment = Experiment(args)
 experiment.load_from_disk()
 
-evaluator = Evaluator(experiment.normalizer)
+evaluator = Evaluator(args.test_starting_point, args.num_total_output_frames, experiment.normalizer,)
 
+logging.info("Generate prediction plots")
+get_sample_predictions(experiment.model, experiment.dataloaders['test'], experiment.device, experiment.dirs['predictions'], experiment.normalizer, args.debug)
 logging.info("Start testing")
-evaluator.compute_experiment_metrics(experiment, args.test_starting_point, args.num_total_output_frames, debug=args.debug)
-# evaluator.save_plots(experiment.dirs['charts'])
-# evaluator.save(experiment.files['evaluator'])
+evaluator.compute_experiment_metrics(experiment, debug=args.debug)
+evaluator.save_metrics_plots(experiment.dirs['charts'])
+evaluator.save_to_file(experiment.files['evaluator'])

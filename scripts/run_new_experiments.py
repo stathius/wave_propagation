21c298639a89957754653d921dce9dfe9e98b263
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-07-06 20:26:29 +0100

    new experiments fixes

--- scripts/run_new_experiments.py
+++ scripts/run_new_experiments.py
@@ -20,13 +20,14 @@ file.close()
 exp_name = '{model_type}_batch_{batch_size}_samples_{samples_per_sequence}_epoch_{num_epochs}_in_{num_input_frames}_out_{num_output_frames}'
 
 for exp, exp_args in new_experiments.items():
+    print(exp, exp_args)
     # for arg, arg_val in exp_args:
     exp_name = '{model_type}_batch_{batch_size}_samples_{samples_per_sequence}_epoch_{num_epochs}_in_{num_input_frames}_out_{num_output_frames}'.format(**exp_args)
-    template = template.format_map(SafeDict(exp_args))
-    template = template.format_map(SafeDict(exp_name=exp_name))
+    template_exp = template.format_map(SafeDict(exp_args))
+    template_exp = template_exp.format_map(SafeDict(exp_name=exp_name))
     exp_script = '%s.sh' % exp_name
     with open(exp_script, 'w') as file:
-        template = file.write(template)
+        file.write(template_exp)
     file.close()
 
-    os.system("sbatch -o %s_%%j.out %s" % (exp_name, exp_script))
+    # os.system("sbatch -o %s_%%j.out %s" % (exp_name, exp_script))

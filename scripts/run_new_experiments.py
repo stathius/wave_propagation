2607079d35cd4412e62a3731e1e274bdda1dcd77
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-07-09 16:24:43 +0100

    adding lr in folder name

--- scripts/run_new_experiments.py
+++ scripts/run_new_experiments.py
@@ -20,7 +20,7 @@ file.close()
 for exp, exp_args in new_experiments.items():
     print(exp, exp_args)
     # for arg, arg_val in exp_args:
-    exp_name = '{model_type}_batch_{batch_size}_samples_{samples_per_sequence}_epoch_{num_epochs}_in_{num_input_frames}_out_{num_output_frames}_normalizer_{normalizer_type}'.format(**exp_args)
+    exp_name = '{model_type}_batch_{batch_size}_samples_{samples_per_sequence}_epoch_{num_epochs}_in_{num_input_frames}_out_{num_output_frames}_normalizer_{normalizer_type}_lr_{learning_rate}'.format(**exp_args)
     template_exp = template.format_map(SafeDict(exp_args))
     template_exp = template_exp.format_map(SafeDict(exp_name=exp_name))
     exp_script = '%s.sh' % exp_name

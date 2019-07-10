583e3093b1461662d9a96faacd91fdb94ff1e9a8
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-07-10 22:44:08 +0100

    renaming test script file to template

--- scripts/test_script_template.sh
+++ scripts/test_script_template.sh
@@ -36,13 +36,4 @@ fi
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 cd /home/s1680171/wave_propagation/
 
-# python test_network.py --experiment_name AR_LSTM_batch_16_samples_10_in_5_out_20_none_lr_0.0001 --test_starting_point 0
-# python test_network.py --experiment_name AR_LSTM_batch_16_samples_10_in_5_out_20_normal_lr_0.0001
-# python test_network.py --experiment_name ResNet_batch_16_samples_5_in_5_out_1_lr_0.001
-# python test_network.py --experiment_name ResNet_batch_16_samples_5_in_5_out_1_lr_0.001_2
-# python test_network.py --experiment_name ConvLSTM_batch_6_samples_5_in_5_out_20_normal_lr_0.001 --test_starting_point 0
-# python test_network.py --experiment_name resnet_batch_16_samples_in_3_out_10_lr_0.001
-
-python test_network.py --experiment_name ar_lstm_batch_16_samples_10_epoch_25_in_5_out_20_normalizer_m1to1_lr_0.0001
-python test_network.py --experiment_name ar_lstm_batch_16_samples_10_epoch_25_in_5_out_20_normalizer_none_lr_0.0001
-python test_network.py --experiment_name ar_lstm_batch_16_samples_10_epoch_25_in_5_out_20_normalizer_normal_lr_0.0001
+python test_network.py {args}

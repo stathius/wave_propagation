03d36dad13a6ae7cb7033c629f58748d3e9388ac
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-07-14 01:34:27 +0100

    using the new evaluation function

--- test_network.py
+++ test_network.py
@@ -1,24 +1,14 @@
 import logging
 import matplotlib.pyplot as plt
 from utils.arg_extract import get_args
-from utils.experiment_evaluator import Evaluator, get_sample_predictions
+from utils.experiment_evaluator import evaluate_experiment
 from utils.experiment import Experiment
 
 plt.ioff()
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 args = get_args()
-args.num_workers = 4
-args.batch_size = 16
 experiment = Experiment(args)
 experiment.load_from_disk(test=True)
 
-evaluator = Evaluator(args.test_starting_point, experiment.normalizer)
-
-logging.info("Start testing")
-evaluator.compute_experiment_metrics(experiment.model, experiment.dataloaders['test'], args.num_total_output_frames, experiment.device, debug=args.debug)
-evaluator.save_metrics_plots(experiment.dirs['charts'])
-evaluator.save_to_file(experiment.files['evaluator'] % args.test_starting_point)
-# Get the sample plots after you compute everything else because the dataloader iterates from the beginning
-logging.info("Generate prediction plots")
-get_sample_predictions(experiment.model, experiment.dataloaders['test'], experiment.device, experiment.dirs['predictions'], experiment.normalizer, args.debug)
+evaluate_experiment(experiment, args)

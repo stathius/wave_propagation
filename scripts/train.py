6fade9a9dcbaa532fe27815b8ce31d3145adda3f
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-07-22 01:28:40 +0100

    adding dataset on exp name

--- scripts/train.py
+++ scripts/train.py
@@ -11,6 +11,11 @@ class SafeDict(dict):
         return '{' + key + '}'
 
 
+# These are with fixed tub, rerun them with original data
+# train_ar_lstm_batch_16_samples_10_in_5_out_20_normal_lr_0.0001.sh
+#   train_convlstm_batch_8_samples_5_in_5_out_10_normal_lr_0.001.sh
+#    train_resnet_batch_16_samples_5_in_5_out_10_normal_lr_0.001.sh
+
 new_experiments = load_json('new_experiments.json')
 
 with open('train.template', 'r') as file:
@@ -20,9 +25,9 @@ file.close()
 for exp, exp_args in new_experiments.items():
     print(exp, exp_args)
     # for arg, arg_val in exp_args:
-    exp_name = '{model_type}_batch_{batch_size}_samples_{samples_per_sequence}_in_{num_input_frames}_out_{num_output_frames}_{normalizer_type}_lr_{learning_rate}'.format(**exp_args)
+    exp_name = '{model_type}_batch_{batch_size}_samples_{samples_per_sequence}_in_{num_input_frames}_out_{num_output_frames}_{normalizer_type}_lr_{learning_rate}_dataset_{dataset}'.format(**exp_args)
 
-    args_template = "--experiment_name {exp_name} --model_type {model_type} --batch_size {batch_size} --num_epochs {num_epochs} --samples_per_sequence {samples_per_sequence} --num_input_frames {num_input_frames} --num_output_frames {num_output_frames} --learning_rate {learning_rate} --normalizer_type {normalizer_type} --num_workers 12"
+    args_template = "--experiment_name {exp_name} --model_type {model_type} --batch_size {batch_size} --num_epochs {num_epochs} --samples_per_sequence {samples_per_sequence} --num_input_frames {num_input_frames} --num_output_frames {num_output_frames} --learning_rate {learning_rate} --normalizer_type {normalizer_type} --dataset {dataset} --num_workers 12"
 
     args_template = args_template.format_map(SafeDict(exp_name=exp_name))
     args_template = args_template.format(**exp_args)
